1decec84f043cb734b03dd66eb6d91bd29076068
genieu99 <yujin0419@sookmyung.ac.kr>
2024-04-23 17:36:26 +0900

    [Feat] practice01-Prob4 해결

--- practice01/src/main/java/prob4/Prob4.java
+++ practice01/src/main/java/prob4/Prob4.java
@@ -7,10 +7,19 @@ public class Prob4 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
-		System.out.print("문자열을 입력하세요 : ");
+		System.out.print("문자열을 입력하세요. : ");
 		String text = scanner.nextLine();
-
 		
+		int count = 1;
+		
+		for (int i = 0; i < text.length(); i++) {
+			for (int j = 0; j < count; j++) {
+				char c = text.charAt(j);
+				System.out.print(c);
+			}
+			System.out.println();
+			count++;
+		}
 		scanner.close();
 	}
 
