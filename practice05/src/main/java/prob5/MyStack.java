c8ea691aaa95df26377c702ef5e35b4f973794ff
genieu99 <yujin0419@sookmyung.ac.kr>
2024-04-26 17:26:27 +0900

    assignment: practice05-prob05 #finish

--- practice05/src/main/java/prob5/MyStack.java
+++ practice05/src/main/java/prob5/MyStack.java
@@ -1,4 +1,38 @@
 package prob5;
 
 public class MyStack {
+	private int maxSize;
+    private String[] stackArray;
+    private int top;
+
+    public MyStack(int size) {
+        this.maxSize = size;
+        this.stackArray = new String[maxSize];
+        this.top = -1;
+    }
+
+    public void push(String value){
+    	if (top >= maxSize - 1) {
+            resize(maxSize * 2);
+        }
+        stackArray[++top] = value;
+    }
+
+    public String pop() throws MyStackException {
+        if (top < 0) {
+            throw new MyStackException("stack is empty");
+        }
+        return stackArray[top--];
+    }
+
+    public boolean isEmpty() {
+        return (top == -1);
+    }
+    
+    private void resize(int newSize) {
+        String[] newArray = new String[newSize];
+        System.arraycopy(stackArray, 0, newArray, 0, maxSize);
+        stackArray = newArray;
+        maxSize = newSize;
+    }
 }
---
+++ practice05/src/main/java/prob5/MyStackException.java
@@ -0,0 +1,8 @@
+package prob5;
+
+public class MyStackException extends Exception {
+
+    public MyStackException(String message) {
+        super(message);
+    }
+}
