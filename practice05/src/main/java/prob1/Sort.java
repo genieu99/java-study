0ce10be86c3ba66f930bd1a57a6b38f85e3150d0
genieu99 <yujin0419@sookmyung.ac.kr>
2024-04-26 17:24:42 +0900

    assignment: practice05-prob01 #finish

--- practice05/src/main/java/prob1/Sort.java
+++ practice05/src/main/java/prob1/Sort.java
@@ -13,10 +13,16 @@ public class Sort {
 			System.out.print( array[ i ] + " " );
 		}
 		
-		//
 		// 정렬 알고리즘이 적용된 코드를 여기에 작성합니다.
-		//
-
+		for (int i = 0; i < count - 1; i++) {
+            for (int j = 0; j < count - i - 1; j++) {
+                if (array[j] < array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
 		
 		// 결과 출력
 		System.out.println( "\nAfter Sort." );
