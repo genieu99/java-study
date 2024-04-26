29218f7a1f59f71577a5c99b229010d8a96b1dab
genieu99 <yujin0419@sookmyung.ac.kr>
2024-04-26 11:32:36 +0900

    class: exception #real finish

--- chapter03/src/main/java/exception/MyException.java
+++ chapter03/src/main/java/exception/MyException.java
@@ -2,7 +2,7 @@ package exception;
 
 @SuppressWarnings("serial")
 public class MyException extends Exception {
-	public MyExcepiton(String message) {
+	public MyException(String message) {
 		super(message);
 	}
 	
