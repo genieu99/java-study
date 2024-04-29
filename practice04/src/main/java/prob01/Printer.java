b2f8f9e8f0cb9b4b8d7ee88c814c6fc86e54f69e
genieu99 <yujin0419@sookmyung.ac.kr>
2024-04-29 13:20:20 +0900

    class: Generic #1

--- practice04/src/main/java/prob01/Printer.java
+++ practice04/src/main/java/prob01/Printer.java
@@ -1,19 +1,39 @@
 package prob01;
 
 public class Printer {
-	public void println(int value) {
-        System.out.println(value);
-    }
-
-    public void println(boolean value) {
-        System.out.println(value);
-    }
-
-    public void println(double value) {
-        System.out.println(value);
-    }
-
-    public void println(String value) {
-        System.out.println(value);
-    }
+//	public void println(int value) {
+//        System.out.println(value);
+//    }
+//
+//    public void println(boolean value) {
+//        System.out.println(value);
+//    }
+//
+//    public void println(double value) {
+//        System.out.println(value);
+//    }
+//
+//    public void println(String value) {
+//        System.out.println(value);
+//    }
+	
+	public <T> void println(T t) {
+		System.out.println(t);
+	}
+	
+	public int sum(Integer... nums) {
+		int s = 0;
+		
+		for (Integer n : nums) {
+			s += n;
+		}
+		
+		return s;
+	}
+	
+	public <T> void println(T... ts) {
+		for (T t : ts) {
+			System.out.println(t);
+		}
+	}
 }
--- practice04/src/main/java/prob01/PrinterTest.java
+++ practice04/src/main/java/prob01/PrinterTest.java
@@ -4,10 +4,20 @@ public class PrinterTest {
 
 	public static void main(String[] args) {
 		Printer printer = new Printer();
-
+		
+		// 원래는 이렇게 적어야 되는데 알아서 확인해줌
+		// printer.<Integer>println(10);
 		printer.println( 10 );
 		printer.println( true );
 		printer.println( 5.7 );
 		printer.println( "홍길동" );
+		
+		System.out.println(printer.sum(1));
+		System.out.println(printer.sum(1, 2, 3));
+		System.out.println(printer.sum(1, 2, 3, 4, 5));
+		System.out.println(printer.sum(1, 2, 3, 4, 5, 6, 7, 8));
+		
+		printer.println(10, "홍길동");
+		printer.println(10, true, "홍길동");
 	}
 }
