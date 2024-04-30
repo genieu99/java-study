f860ca07e31f6a0d6ea8d48f868f8b00bee78a01
genieu99 <yujin0419@sookmyung.ac.kr>
2024-04-30 11:27:55 +0900

    class: decorator pattern

---
+++ designpattern-practices/src/main/java/structural/decorator/Client.java
@@ -0,0 +1,14 @@
+package structural.decorator;
+
+public class Client {
+
+	public static void main(String[] args) {
+		System.out.println(new ConcreteComponent("Hello World").operation());
+		System.out.println(new BracesDecorator(new ConcreteComponent("Hello World")).operation());
+		System.out.println(new ParenthesesDecorator(new BracesDecorator(new ConcreteComponent("Hello World"))).operation());
+		
+		System.out.println(new ParenthesesDecorator(new ConcreteComponent("Hello World")).operation());
+		System.out.println(new BracesDecorator(new ParenthesesDecorator(new ConcreteComponent("Hello World"))).operation());
+	}
+
+}
