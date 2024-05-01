762f339a8160025f12d9c91c006713a1eb7abe0f
genieu99 <yujin0419@sookmyung.ac.kr>
2024-05-01 14:44:54 +0900

    class: TCP Server #finish

--- network/src/main/java/test/TCPServer.java
+++ network/src/main/java/test/TCPServer.java
@@ -1,9 +1,12 @@
 package test;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 
 public class TCPServer {
 
@@ -11,19 +14,60 @@ public class TCPServer {
 		ServerSocket serverSocket = null;
 		
 		try {
-			// 1. Server Socket 생
+			// 1. Server Socket 생성
 			serverSocket = new ServerSocket();
 			
 			// 2. 바인딩(binding)
 			//    Socket에 InetsocketAddress[InetAddress(IPAddress) + Port]를 바인딩 한다.
 			//    IPAddress: 0.0.0.0: 특정 호스트 IP를 바인딩 하지 않는다.
-			serverSocket.bind(new InetSocketAddress("0.0.0.0", 5000), 10);
+			serverSocket.bind(new InetSocketAddress("0.0.0.0", 5001), 10);
 			
 			// 3. accept
 			Socket socket = serverSocket.accept();  // blocking
 			
-			System.out.println("연결!!")
-			
+			try {
+				InetSocketAddress inetRemoteSocketAddress = (InetSocketAddress)socket.getRemoteSocketAddress();
+				String remoteHostAddress = inetRemoteSocketAddress.getAddress().getHostAddress();			
+				int remotePort = inetRemoteSocketAddress.getPort();
+				
+				System.out.println("연결!!");
+				System.out.println("[server] connected by client[" + remoteHostAddress + ":" + remotePort + "]");
+				
+				// 4. IO Stream 받아오기
+				InputStream is = socket.getInputStream();
+				OutputStream os = socket.getOutputStream();
+				
+				while(true) {
+					// 5. 데이터 읽기
+					byte[] buffer = new byte[256];
+					int readByteCount = is.read(buffer);  // blocking
+					
+					if (readByteCount == -1) {
+						// client가 정상적으로 종료(close() 호출)
+						System.out.println("[server] closed by clinet");
+						break;
+					}
+					
+					String data = new String(buffer, 0, readByteCount, "UTF-8");
+					System.out.println("[server] received: " + data);
+					
+					// 6. 데이터 쓰기
+					os.write(data.getBytes("utf-8"));
+				}
+			} catch (SocketException e) {
+				System.out.println("[server] suddenly closed by clinet");
+			} catch (IOException e) {
+				System.out.println("[server] error: " + e);
+			} finally {
+				try {
+					if (serverSocket != null && !serverSocket.isClosed()) {
+						serverSocket.close();
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+	
 		} catch (IOException e) {
 			System.out.println("[server] error: " + e);
 		} finally {
@@ -35,7 +79,6 @@ public class TCPServer {
 				e.printStackTrace();
 			}
 		}
-
 	}
 
 }
