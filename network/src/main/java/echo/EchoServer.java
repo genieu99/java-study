ee8dddc4d953fde1ecb3f6a14bb03a802ceccdd2
genieu99 <yujin0419@sookmyung.ac.kr>
2024-05-02 10:51:22 +0900

    class: echo c/s #multi-thread

--- network/src/main/java/echo/EchoServer.java
+++ network/src/main/java/echo/EchoServer.java
@@ -18,44 +18,13 @@ public class EchoServer {
 		
 		try {
 			serverSocket = new ServerSocket();
-			
 			serverSocket.bind(new InetSocketAddress("0.0.0.0", PORT), 10);
-			Socket socket = serverSocket.accept();  // blocking
+			log("starts....[port: " + PORT + "]");
 			
-			try {
-				InetSocketAddress inetRemoteSocketAddress = (InetSocketAddress)socket.getRemoteSocketAddress();
-				String remoteHostAddress = inetRemoteSocketAddress.getAddress().getHostAddress();			
-				int remotePort = inetRemoteSocketAddress.getPort();
-				log("connected by client[" + remoteHostAddress + ":" + remotePort + "]");
-				
-				PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "utf-8"), true);
-				BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "utf-8"));
-				
-				while(true) {
-					String data = br.readLine();  // blocking
-					
-					if (data == null) {
-						log("closed by clinet");
-						break;
-					}
-					
-					log("received: " + data);
-					pw.println(data);
-				}
-			} catch (SocketException e) {
-				log("Socket Exception: " + e);
-			} catch (IOException e) {
-				log("error: " + e);
-			} finally {
-				try {
-					if (serverSocket != null && !serverSocket.isClosed()) {
-						serverSocket.close();
-					}
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
+			while(true) {
+				Socket socket = serverSocket.accept();  // blocking
+				new EchoRequestHandler(socket).start();
 			}
-	
 		} catch (IOException e) {
 			log("error: " + e);
 		} finally {
@@ -69,7 +38,7 @@ public class EchoServer {
 		}
 	}
 	
-	private static void log(String message) {
+	public static void log(String message) {
 		System.out.println("[EchoServer] " + message);
 	}
 }
---
+++ network/src/main/java/thread/ThreadEx01.java
@@ -0,0 +1,19 @@
+package thread;
+
+public class ThreadEx01 {
+
+	public static void main(String[] args) {
+		new DigitThread().start();
+		
+		for (char c = 'a'; c < 'k'; c++) {
+			System.out.print(c);
+			
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
