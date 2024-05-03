bc2e471ea9ad284dd08d599ce937f74069bea245
genieu99 <yujin0419@sookmyung.ac.kr>
2024-05-03 10:38:04 +0900

    class: TCP Algorithm & Socket Options

--- network/src/main/java/test/TCPServer.java
+++ network/src/main/java/test/TCPServer.java
@@ -17,6 +17,10 @@ public class TCPServer {
 			// 1. Server Socket 생성
 			serverSocket = new ServerSocket();
 			
+			// 1-1. FIN_WAIT2 -> TIME_WAIT 상태에서도 소켓 포트 할당이 가능하도록 하기 위해
+			serverSocket.setReuseAddress(true);
+			
+			
 			// 2. 바인딩(binding)
 			//    Socket에 InetsocketAddress[InetAddress(IPAddress) + Port]를 바인딩 한다.
 			//    IPAddress: 0.0.0.0: 특정 호스트 IP를 바인딩 하지 않는다.
@@ -52,6 +56,13 @@ public class TCPServer {
 					System.out.println("[server] received: " + data);
 					
 					// 6. 데이터 쓰기
+					// SO_TIMEOUT 테스트
+//					try {
+//						Thread.sleep(3000);
+//					} catch (InterruptedException e) {
+//						e.printStackTrace();
+//					}
+					
 					os.write(data.getBytes("utf-8"));
 					
 					try {
